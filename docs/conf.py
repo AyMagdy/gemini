0a2a72e895e83d871ee1e977b182e31d04db24ef
ayman <aymanmagdy.kh@gmail.com>
2024-07-01 03:57:37 +0300

    committing the whole repo

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.viewcode','sphinx.ext.autodoc','enum_tools.autoenum']
+extensions = ['sphinx.ext.viewcode','sphinx.ext.autodoc','enum_tools.autoenum',]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
